78a59afb34d20ceee9b4adb8b029703860bb8383
Laverne Rollison <127982293+LaverneRollison@users.noreply.github.com>
2025-09-14 02:34:13 +0200

    Revert "Billing & ServiceOrder- domain factory repository service"
    This reverts commit 503c3d9747fc45a74a21d98006bff365a31e13bf.

--- BillingRepository.java
+++
@@ -1,8 +0,0 @@
-package za.ac.cput.carservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import za.ac.cput.carservice.domain.Billing;
-
-
-public interface BillingRepository extends JpaRepository<Billing, Long> {
-}
--- ServiceOrderRepository.java
+++
@@ -1,7 +0,0 @@
-package za.ac.cput.carservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import za.ac.cput.carservice.domain.ServiceOrder;
-
-public interface ServiceOrderRepository extends JpaRepository<ServiceOrder, Long> {
-}
